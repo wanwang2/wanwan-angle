729b0a14dd81cfedb1dcc82aafb95cd719681f80
[wanwang2] <632468635@qq.com>
2017-01-11 21:51:11 +0800

    vv

---
+++ src/main/java/org/wanwanframework/angle/spiritgirl/SpiritgirlController.java
@@ -0,0 +1,18 @@
+package org.wanwanframework.angle.spiritgirl;
+
+public class SpiritgirlController {
+
+	/**
+	 * 创建文件结构
+	 */
+	public void makeFileStruct() {
+		
+	}
+	
+	/**
+	 * 创建文件内容
+	 */
+	public void makeFileContent() {
+		
+	}
+}
