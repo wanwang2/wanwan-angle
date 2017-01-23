4df1fc86eb7133b050dd1a970d854069a955656b
[wanwang2] <632468635@qq.com>
2017-01-23 23:20:13 +0800

    vv

--- src/main/java/org/wanwanframework/angle/spiritgirl/SpiritgirlController.java
+++ src/main/java/org/wanwanframework/angle/spiritgirl/SpiritgirlController.java
@@ -1,7 +1,10 @@
 package org.wanwanframework.angle.spiritgirl;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
+import org.wanwanframwork.file.FileUtil;
 import org.wanwanframwork.file.Log;
 import org.wanwanframwork.file.util.MappingUtil;
 
@@ -29,6 +32,22 @@ public class SpiritgirlController {
 		Map<String, String>[] mapArray = MappingUtil.getMapping(resources, ":\t");
 		
 		Log.log(mapArray);
+	}
+	
+	private void processFile(String key, String value) {
+		
+		if(key.contains("@")) {
+			String keys[] = key.split("@");
+			if(keys[1].indexOf("file") >= 0) {
+				try {
+					FileUtil.makeFile(value);
+				} catch (IOException e) { 
+					e.printStackTrace();
+				}
+			} else if(keys[1].indexOf("folder") >= 0) {
+				FileUtil.makeFolder(value);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
