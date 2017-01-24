c613a2b82bfc91b35938451bb0192f0ed0e813f5
[wanwang2] <632468635@qq.com>
2017-01-24 13:10:05 +0800

    vv

--- src/main/java/org/wanwanframework/angle/spiritgirl/SpiritgirlController.java
+++ src/main/java/org/wanwanframework/angle/spiritgirl/SpiritgirlController.java
@@ -1,6 +1,5 @@
 package org.wanwanframework.angle.spiritgirl;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -8,21 +7,18 @@ import org.wanwanframwork.file.FileUtil;
 import org.wanwanframwork.file.Log;
 import org.wanwanframwork.file.util.MappingUtil;
 
+/**
+ * 分发格式：
+ * file->path
+ * 		|content
+ * 
+ * fileList->list<file>
+ * 
+ * 
+ */
 public class SpiritgirlController {
 
-	/**
-	 * 创建文件结构
-	 */
-	public void makeFileStruct() {
-		
-	}
-	
-	/**
-	 * 创建文件内容
-	 */
-	public void makeFileContent() {
-		
-	}
+	private Map<String, String> param;
 	
 	public void init() {
 		
@@ -30,10 +26,47 @@ public class SpiritgirlController {
 				"./src/main/resources/spirit/param/param.txt",
 				"./src/main/resources/spirit/filelist.txt"};
 		Map<String, String>[] mapArray = MappingUtil.getMapping(resources, ":\t");
-		
+		param = mapArray[0];
+		processFileStructure(mapArray[1]);
 		Log.log(mapArray);
 	}
 	
+	/**
+	 * 替换元数据
+	 */
+	private String processFilter(String content) {
+		String value;
+		for(String key:param.keySet()) {
+			value = param.get(key);
+			content = filter(content, key, value);
+			Log.log("key:" + key + ", value:" + value);
+		}
+		return content;
+	}
+	
+	private String filter(String content, String key, String value) {
+		return content = content.replaceAll(key, value);
+	}
+	
+	/**
+	 * 处理文件系统
+	 * @param map
+	 */
+	private void processFileStructure(Map<String, String> map) {
+		String value;
+		for(String key:map.keySet()) {
+			value = map.get(key);
+			value = processFilter(value);
+			processFile(key, value);
+			Log.log("key:" + key + ", value:" + value);
+		}
+	}
+	
+	/**
+	 * 处理单个文件或者目录
+	 * @param key
+	 * @param value
+	 */
 	private void processFile(String key, String value) {
 		
 		if(key.contains("@")) {
