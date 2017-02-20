e0f0bf991bb93a17137e556d5ad3090178f0c565
[wanwang2] <1053400545@qq.com>
2017-02-20 23:16:07 +0800

    vv

--- src/main/java/org/wanwanframework/angle/sequencemapping/SequenceMapping.java
+++ src/main/java/org/wanwanframework/angle/sequencemapping/SequenceMapping.java
@@ -2,4 +2,14 @@ package org.wanwanframework.angle.sequencemapping;
 
 public class SequenceMapping {
 
+	private String post;
+	private String struts;
+	
+	public void toPost(String post) {
+		
+	}
+	
+	public void toStruts(String struts) {
+		
+	}
 }
